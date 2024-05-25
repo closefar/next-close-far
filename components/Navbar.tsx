9d9ec3a07fa7ab01188802bafe3331e5fa90c715
Ali Ashiri <ali.as.arc.s@gmail.com>
2024-05-25 11:55:01 +0000

    add user authentications

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,21 +3,29 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import BreakLine from "./BreakLine";
+import useCurrentUser from "../hooks/useCurrentUser";
+import * as fcl from "@onflow/fcl";
 
 const Navbar = () => {
-  const currentUser = true;
+  const currentUser = useCurrentUser();
+  // const currentUser = true;
+  console.log(currentUser);
   return (
     <>
       <div className="grid grid-cols-3 px-28 py-3 ">
-        <div className="flex items-center justify-start">currentUser</div>
+        <div className="flex items-center justify-start">
+          {currentUser.addr}
+        </div>
         <div className="flex items-center justify-center">
           <Link href="/">
             <Image src={"/logo3.jpg"} alt="" width={100} height={100} />
           </Link>
         </div>
         <ul className="flex items-center justify-end gap-5">
-          {!currentUser ? (
-            <li>Connect Wallet</li>
+          {!currentUser.loggedIn ? (
+            <li onClick={fcl.authenticate} className="cursor-pointer">
+              Connect Wallet
+            </li>
           ) : (
             <>
               <li className="min-w-fit">
@@ -29,7 +37,12 @@ const Navbar = () => {
               <li className="min-w-fit">
                 <Link href="/my-collections">My Collection</Link>
               </li>
-              <li className="min-w-fit">Log Out</li>
+              <li
+                className="min-w-fit cursor-pointer"
+                onClick={fcl.unauthenticate}
+              >
+                Log Out
+              </li>
             </>
           )}
         </ul>
